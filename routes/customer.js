a0d404b80a46fbd795816de3fd2e408d5e6fa634
mats <mats@coral.plala.or.jp>
2023-01-21 16:45:34 +0900

    PUT and DELETE method

--- routes/customer.js
+++ routes/customer.js
@@ -21,7 +21,22 @@ router.post("/", (req, res) => {
         id: customers.length +1,
     };
     customers.push(customer);
-    res.send(customers);
+    res.send(customer);
+});
+
+//PUTメソッド
+router.put("/:id", (req, res) => {
+    const customer = customers.find((c) => c.id === parseInt(req.params.id));
+    customer.name = req.body.name;
+    res.send(customer);
+});
+
+//DELETEメソッド
+router.delete("/:id", (req, res) => {
+    const customer = customers.find((c) => c.id === parseInt(req.params.id));
+    const index = customers.indexOf(customer);
+    customers.splice(index, 1);
+    res.send(customer);
 });
 
 module.exports = router;
