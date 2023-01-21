58e8cc9066952e952a6415fed551c3ec6b7fab84
mats <mats@coral.plala.or.jp>
2023-01-21 12:25:21 +0900

    add get method

--- server.js
+++ server.js
@@ -2,4 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    //console.log("hello express");
+    //res.send("<h1>こんにちは</h1>");
+    //res.sendStatus(404);
+    //res.status(500).send("エラーです");
+    res.status(500).json({msg: "エラーです"})
+});
+
+app.get("/user", (req, res) => {
+    res.send("ユーザです");
+});
+
+app.get("/user/info", (req, res) => {
+    res.send("ユーザ情報です");
+});
+
 app.listen(PORT, () => console.log("サーバが起動しました"));
